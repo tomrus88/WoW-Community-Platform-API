551bb406221eb0c4565ab5a649b19dc049e2c223
TOM_RUS <tom_rus@getmangos.com>
2011-05-11 23:40:18 +0400

    Added regions menu,

--- WowRealmStatus/Form1.Designer.cs
+++ WowRealmStatus/Form1.Designer.cs
@@ -29,12 +29,23 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
@@ -49,13 +60,24 @@
             this.columnHeader2,
             this.columnHeader3,
             this.columnHeader4});
-            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Location = new System.Drawing.Point(12, 27);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(603, 637);
+            this.listView1.Size = new System.Drawing.Size(603, 622);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Name";
+            this.columnHeader5.Width = 150;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Slug";
+            this.columnHeader6.Width = 150;
             // 
             // columnHeader1
             // 
@@ -77,15 +99,96 @@
             this.columnHeader4.Text = "Status";
             this.columnHeader4.Width = 50;
             // 
-            // columnHeader5
+            // menuStrip1
             // 
-            this.columnHeader5.Text = "Name";
-            this.columnHeader5.Width = 150;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(628, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // columnHeader6
+            // fileToolStripMenuItem
             // 
-            this.columnHeader6.Text = "Slug";
-            this.columnHeader6.Width = 150;
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
+            this.regionToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // regionToolStripMenuItem
+            // 
+            this.regionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eUToolStripMenuItem,
+            this.uSToolStripMenuItem,
+            this.kRToolStripMenuItem,
+            this.tWToolStripMenuItem});
+            this.regionToolStripMenuItem.Name = "regionToolStripMenuItem";
+            this.regionToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.regionToolStripMenuItem.Text = "Region";
+            // 
+            // eUToolStripMenuItem
+            // 
+            this.eUToolStripMenuItem.Checked = true;
+            this.eUToolStripMenuItem.CheckOnClick = true;
+            this.eUToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.eUToolStripMenuItem.Name = "eUToolStripMenuItem";
+            this.eUToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.eUToolStripMenuItem.Tag = "eu";
+            this.eUToolStripMenuItem.Text = "EU";
+            this.eUToolStripMenuItem.Click += new System.EventHandler(this.regionToolStripMenuItem_Click);
+            // 
+            // uSToolStripMenuItem
+            // 
+            this.uSToolStripMenuItem.CheckOnClick = true;
+            this.uSToolStripMenuItem.Name = "uSToolStripMenuItem";
+            this.uSToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.uSToolStripMenuItem.Tag = "us";
+            this.uSToolStripMenuItem.Text = "US";
+            this.uSToolStripMenuItem.Click += new System.EventHandler(this.regionToolStripMenuItem_Click);
+            // 
+            // kRToolStripMenuItem
+            // 
+            this.kRToolStripMenuItem.CheckOnClick = true;
+            this.kRToolStripMenuItem.Name = "kRToolStripMenuItem";
+            this.kRToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.kRToolStripMenuItem.Tag = "kr";
+            this.kRToolStripMenuItem.Text = "KR";
+            this.kRToolStripMenuItem.Click += new System.EventHandler(this.regionToolStripMenuItem_Click);
+            // 
+            // tWToolStripMenuItem
+            // 
+            this.tWToolStripMenuItem.CheckOnClick = true;
+            this.tWToolStripMenuItem.Name = "tWToolStripMenuItem";
+            this.tWToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.tWToolStripMenuItem.Tag = "tw";
+            this.tWToolStripMenuItem.Text = "TW";
+            this.tWToolStripMenuItem.Click += new System.EventHandler(this.regionToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -93,10 +196,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(628, 661);
             this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "WowRealmStatus";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,6 +217,16 @@
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.ColumnHeader columnHeader5;
         private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eUToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tWToolStripMenuItem;
     }
 }
 
--- WowRealmStatus/Form1.cs
+++ WowRealmStatus/Form1.cs
@@ -8,27 +8,41 @@ namespace WowRealmStatus
 {
     public partial class Form1 : Form
     {
-        private ListViewColumnSorter lvwColumnSorter;
+        private ListViewColumnSorter columnSorter;
+        private string m_region;
 
         public Form1()
         {
             InitializeComponent();
 
-            // Create an instance of a ListView column sorter and assign it 
-            // to the ListView control.
-            lvwColumnSorter = new ListViewColumnSorter();
-            listView1.ListViewItemSorter = lvwColumnSorter;
+            // Create an instance of a ListView column sorter and assign it to the ListView control.
+            columnSorter = new ListViewColumnSorter();
+            listView1.ListViewItemSorter = columnSorter;
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            m_region = "eu";
+
+            FillListView();
+        }
+
+        private void FillListView()
+        {
             WebClient client = new WebClient();
+            client.DownloadDataCompleted += new DownloadDataCompletedEventHandler(client_DownloadDataCompleted);
+            client.DownloadDataAsync(new Uri(String.Format("http://{0}.battle.net/api/wow/realm/status", m_region)));
+        }
+
+        void client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            listView1.Items.Clear();
 
             try
             {
-                var data = client.DownloadData("http://eu.battle.net/api/wow/realm/status");
-
-                var dataStr = Encoding.UTF8.GetString(data);
+                var dataStr = Encoding.UTF8.GetString(e.Result);
 
                 var serializer = new JavaScriptSerializer();
                 var status = serializer.Deserialize<RealmStatus>(dataStr);
@@ -39,33 +53,40 @@ namespace WowRealmStatus
             catch (Exception exc)
             {
                 MessageBox.Show(exc.Message);
+                if (exc.InnerException != null)
+                    MessageBox.Show(exc.InnerException.Message);
             }
         }
 
         private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
         {
-            // Determine if clicked column is already the column that is being sorted.
-            if (e.Column == lvwColumnSorter.SortColumn)
+            columnSorter.SortColumn = e.Column;
+            listView1.Sorting = listView1.Sorting == SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending;
+            columnSorter.Order = listView1.Sorting;
+            listView1.Sort();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FillListView();
+        }
+
+        private void regionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ToolStripMenuItem region in regionToolStripMenuItem.DropDownItems)
             {
-                // Reverse the current sort direction for this column.
-                if (lvwColumnSorter.Order == SortOrder.Ascending)
-                {
-                    lvwColumnSorter.Order = SortOrder.Descending;
-                }
+                if (region != sender)
+                    region.Checked = false;
                 else
-                {
-                    lvwColumnSorter.Order = SortOrder.Ascending;
-                }
-            }
-            else
-            {
-                // Set the column number that is to be sorted; default to ascending.
-                lvwColumnSorter.SortColumn = e.Column;
-                lvwColumnSorter.Order = SortOrder.Ascending;
+                    m_region = (string)region.Tag;
             }
 
-            // Perform the sort with these new sort options.
-            listView1.Sort();
+            FillListView();
         }
     }
 }
