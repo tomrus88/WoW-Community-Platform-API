afc62d9cbeeb15557719640c4f3230edd8a6e7ce
TOM_RUS <tom_rus@getmangos.com>
2011-05-12 14:03:38 +0400

    Added Character API (NYI).

---
+++ WowCharacterInfo/CharacterInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace WowCharacterInfo
+{
+    class Character
+    {
+        const string baseURL = "http://{0}.battle.net/api/wow/character/{1}/{2}";
+
+        public static Character Get(string region, string realm, string character)
+        {
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    byte[] data;
+
+                    string args = String.Empty;
+
+                    data = client.DownloadData(new Uri(String.Format(baseURL, region, realm, character)));
+
+                    var dataStr = Encoding.UTF8.GetString(data);
+
+                    var serializer = new JavaScriptSerializer();
+
+                    return serializer.Deserialize<Character>(dataStr);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ WowCharacterInfo/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WowCharacterInfo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var character = Character.Get("us", "Uther", "Tieb");
+
+            if (character == null)
+                MessageBox.Show("Failed!");
+        }
+    }
+}
